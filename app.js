c2127fb6596b0028988ec2c8799f1830205c0c34
NizamMuhammed <mohammednizampn@gmail.com>
2024-03-12 23:19:44 +0000

    add media queries

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
-const sections = document.querySelectorAll(".section")
-const sectionButtons = document.querySelectorAll(".controls")
-const sectionButton = document.querySelectorAll(".control")
-const allSections = document.querySelector(".main-content")
-
-function PageTransitions() {
-  //Button click active, used to toggle active button in navigation control
-  for (let i = 0; i < sectionButton.length; i++) {
-    sectionButton[i].addEventListener("click", function () {
-      let currentButton = document.querySelector(".active-btn")
-      currentButton.classList.remove("active-btn")
-      this.classList.add("active-btn")
-
-      //remove and replace active section
-      sections.forEach((section) => {
-        section.classList.remove("active")
-      })
-
-      const currentId = this.dataset.id
-      const selectedSection = document.getElementById(currentId)
-      selectedSection.classList.add("active")
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
     })
-  }
-}
-
-PageTransitions()
+})();
